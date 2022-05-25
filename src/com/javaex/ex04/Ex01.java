4695a1618607ee7f4504fca743c9656e1dd3e580
qpt89 <qpt89@DESKTOP-BSF8PLI>
2022-05-25 23:58:17 +0900

    .

---
+++ src/com/javaex/ex04/Ex01.java
@@ -0,0 +1,18 @@
+package com.javaex.ex04;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+
+		int i;
+		int sum = 0;
+
+		for (i = 1; i <= 10; i++) {
+			sum = sum + i;
+
+			System.out.println(sum);
+		}
+
+	}
+
+}
---
+++ src/com/javaex/ex04/Ex03.java
@@ -0,0 +1,22 @@
+package com.javaex.ex04;
+
+public class Ex03 {
+
+	public static void main(String[] args) {
+
+		int a;
+		int b;
+
+		for (a = 2; a < 10; a++) {
+
+			for (b = 1; b < 10; b++) {
+
+				System.out.println(a + " * " + b + " = " + a * b);
+
+			}
+
+		}
+
+	}
+
+}
