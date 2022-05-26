bbed4fcfba0667af0f30db11da5d9fec35feda47
kyseul513 <kyseul513@hanmail.net>
2022-05-26 23:28:48 +0900

    .

---
+++ src/com/javaex/ex04/Ex07.java
@@ -0,0 +1,19 @@
+package com.javaex.ex04;
+
+public class Ex07 {
+
+	public static void main(String[] args) {
+		
+		int[] iArray = new int[3];
+			iArray[0] = 1;
+			iArray[1] = 2;
+			iArray[2] = 3;
+		
+		
+		int[] jArray = new int[] {1,2,3};
+		int[] kArray = {1,2,3};
+		
+		
+	}
+
+}
