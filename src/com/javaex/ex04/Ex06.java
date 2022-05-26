961f40154b7788f0c32089b590d0072ec87034d7
qpt89 <qpt89@DESKTOP-BSF8PLI>
2022-05-26 15:40:09 +0900

    .

--- src/com/javaex/ex04/Ex06.java
+++ src/com/javaex/ex04/Ex06.java
@@ -4,11 +4,11 @@ public class Ex06 {
 
 	public static void main(String[] args) {
 
-		for (int i=1; i<=100; i++) {
+		for (int i = 1; i <= 100; i++) {
 
 			if ((i % 6 == 0) && (i % 14 == 0)) {
 				System.out.println(i);
-				
+
 				break;
 			}
 		}
