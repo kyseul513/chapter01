c05262fa9a1f565e470a0e8086aa48324800a785
kyseul513 <kyseul513@hanmail.net>
2022-05-31 02:05:52 +0900

    .

--- src/com/javaex/ex04/Ex07.java
+++ src/com/javaex/ex04/Ex07.java
@@ -1,6 +1,7 @@
 package com.javaex.ex04;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Ex07 {
@@ -22,7 +23,8 @@ public class Ex07 {
 		
 		
 		// 컬렉션 프레임워크 - List 컬렉션
-		// 1. ArrayList
+		// 1-1. ArrayList
+		// 저장공간을 초과한 객체들이 들어오면 자동적으로 저장 용량이 늘어남.
 		List<String> list = new ArrayList<String>();
 			list.add("Java");
 			list.add("JDBC");
@@ -35,11 +37,24 @@ public class Ex07 {
 		String skill = list.get(1);
 		System.out.println("1번 인덱스: " + skill);
 		
+		
 		for(int i=0; i<list.size(); i++) {
 			String str = list.get(i);
 			System.out.println(i + " : " + str);
 		}
-	
+		
+		// 1-2. asList
+		// 고정된 객체들로 구성된 List.
+		List<String> alist1 = Arrays.asList("Java", "JDBC");
+		for(String name: alist1) {
+			System.out.println("alist1: "+name);
+		}
+		
+		
+		List<Integer> alist2 = Arrays.asList(1, 2, 3);
+		for(int number: alist2) {
+			System.out.println("alist2: "+number);
+		}
 		
 	
 	}
