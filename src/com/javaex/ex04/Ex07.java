1c93fe0bda41d9b5e15e825724b7f5e21e132f26
kyseul513 <kyseul513@hanmail.net>
2022-05-30 23:18:46 +0900

    .

--- src/com/javaex/ex04/Ex07.java
+++ src/com/javaex/ex04/Ex07.java
@@ -15,6 +15,11 @@ public class Ex07 {
 		int[] jArray = new int[] { 1, 2, 3 };
 		int[] kArray = { 1, 2, 3 };
 
+		int ilength = iArray.length; //배열의 길이를 알고자 할때 length 사용.
+		System.out.println("총 객체수: " + ilength);
+		System.out.println("총 객체수: " + iArray.length);
+		
+		
 		
 		// 컬렉션 프레임워크 - List 컬렉션
 		// 1. ArrayList
@@ -22,11 +27,18 @@ public class Ex07 {
 			list.add("Java");
 			list.add("JDBC");
 			
-		int size = list.size();
+		int size = list.size();	//컬렉션프레임워크 타입의 길이를 알고자 할때 size 사용.
 		System.out.println("총 객체수: " + size);
-	
+		System.out.println("총 객체수: " + list.size());
+		
+		
 		String skill = list.get(1);
 		System.out.println("1번 인덱스: " + skill);
+		
+		for(int i=0; i<list.size(); i++) {
+			String str = list.get(i);
+			System.out.println(i + " : " + str);
+		}
 	
 		
 	
